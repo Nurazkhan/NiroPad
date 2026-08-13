f68187d729d49a3be1385d661e4e78440d811f11
Nurazkhan Nygmet <rominefire@gmail.com>
2026-08-14 00:17:05 +0500

    production folder fix

--- firmware/config.h
+++ firmware/config.h
@@ -3,10 +3,31 @@
 #define I2C1_SDA_PIN GP6
  #define I2C1_SCL_PIN GP7
 #define RGBLED_NUM 20
+// Ensure RGBLIGHT_LED_COUNT is available for drivers that expect it
+#ifndef RGBLIGHT_LED_COUNT
+#define RGBLIGHT_LED_COUNT RGBLED_NUM
+#endif
+
+// USB identity (required by the USB descriptor)
+// You can change these to your actual VID/PID if you have them.
+#ifndef VENDOR_ID
+#define VENDOR_ID 0xFEED
+#endif
+#ifndef PRODUCT_ID
+#define PRODUCT_ID 0x6060
+#endif
+#ifndef DEVICE_VER
+#define DEVICE_VER 0x0001
+#endif
+#ifndef MANUFACTURER
+#define MANUFACTURER "Nuraz"
+#endif
+#ifndef PRODUCT
+#define PRODUCT "NiroPad"
+#endif
 
 #define ENCODER_A_PINS { GP29 }
 #define ENCODER_B_PINS { GP3 }
 
 #define WS2812_DI_PIN GP0
-#define RGBLIGHT_LED_COUNT 20
 
